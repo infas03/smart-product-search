24470320e8fbb66d659761a9da3bd50e75dc86d6
infas03 <infas@mayaglobal.io>
2026-03-30 23:44:15 +0530

    feat: base project setup, server and client

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-2xl font-bold text-center pt-10">
+        Smart Product Search
+      </h1>
+    </div>
+  );
+}
+
+export default App;
